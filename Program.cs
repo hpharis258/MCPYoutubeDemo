7ce006485f356d2fc80e2ae42c95e1c89d94d1af
Haroldas Varanauskas <hpharis258@gmail.com>
2025-04-19 13:01:25 +0100

    Youtube Demo Source code

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System;
+using System.Speech.Synthesis;
+using System.Diagnostics;
+
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+// config services
+builder.Services.AddMcpServer().WithStdioServerTransport().WithToolsFromAssembly();
+
+// run
+await builder.Build().RunAsync();
+
+// MCP Server tools
+[McpServerToolType]
+public static class MyTools
+{
+    [McpServerTool, Description("Reverses the input string.")]
+    public static string Reverse(string message) => (string)message.Reverse();
+
+    [McpServerTool, Description("Converts the input to synthesized speech, this only works on windows.")]
+    public static void SpeakWindows(string message)
+    {
+        using (var synthesizer = new SpeechSynthesizer())
+        {
+            synthesizer.Speak(message);
+        }
+    }
+    [McpServerTool, Description("Converts the input to synthesized speech, this only works on MacOs.")]
+    public static void SpeakMacOS(string message)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "say",
+            Arguments = message,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        });
+    }
+}
